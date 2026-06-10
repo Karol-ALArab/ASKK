0548b23e82da723afd4875145687b1780560a5d5
Kevin Qazim <W0883375@myscc.ca>
2026-06-10 16:01:06 -0400

    FEATURE: Added lazy loading on all components except home

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter, Routes } from '@angular/router';
+import { Home } from './app/pages/home/home';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// defining all routes and setting up lazy loading
+const routes: Routes = [
+
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: Home }, // eager load home page
+  { path: 'about', loadComponent: () => import('./app/pages/about/about').then((m) => m.About) },
+  { path: 'contact', loadComponent: () => import('./app/pages/contact/contact').then((m) => m.Contact) },
+  { path: 'projects', loadComponent: () => import('./app/pages/projects/projects').then((m) => m.Projects) },
+  { path: 'services', loadComponent: () => import('./app/pages/services/services').then((m) => m.Services) },
+  {path: '**', component: Home}
+];
+
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)],
+}).then((r) => console.log('Bootstrap successful'));
