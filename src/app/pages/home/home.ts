b9eb695937e5abd383d576c5c14e3bad9db5a1eb
Kevin Qazim <W0883375@myscc.ca>
2026-07-13 19:20:57 -0400

    #19 general skeleton of the home page complete

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { FadeInDirective } from '../../directives/fade-in';
 import { MatDivider } from '@angular/material/list';
+import { NgOptimizedImage } from '@angular/common';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { HoverAnim } from '../../directives/hover-anim';
 
 @Component({
   selector: 'app-home',
-  imports: [FadeInDirective, MatDivider],
+  imports: [
+    FadeInDirective,
+    MatDivider,
+    NgOptimizedImage,
+    MatCardHeader,
+    MatCard,
+    MatCardSubtitle,
+    MatCardTitle,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
+    HoverAnim,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
