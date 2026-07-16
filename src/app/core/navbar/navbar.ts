68147f5048255459e48414706675b5559e2c43ad
Kevin Qazim <W0883375@myscc.ca>
2026-07-16 13:09:20 -0400

    #31 FEAT: Added mat button design to nav bar links Made logo in nav bar clickable for home naviagation

--- src/app/core/navbar/navbar.ts
+++ src/app/core/navbar/navbar.ts
@@ -4,10 +4,11 @@ import { MatToolbar } from '@angular/material/toolbar';
 import { MatButton } from '@angular/material/button';
 import { MatDivider } from '@angular/material/list';
 import { MatCard } from '@angular/material/card';
+import { HoverAnim } from '../../directives/hover-anim';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLinkActive, RouterLink, MatToolbar, MatButton, MatDivider, MatCard],
+  imports: [RouterLinkActive, RouterLink, MatToolbar, MatButton, MatDivider, MatCard, HoverAnim],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
