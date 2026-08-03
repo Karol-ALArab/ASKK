b347da822951b8c873bcec38895d14698302455f
karolAL <karolalarab3@gmail.com>
2026-08-03 02:46:12 -0400

    Auto-generated commit

---
+++ chunk-2D4IPA3U.js
@@ -0,0 +1,1 @@
+import{b as a,c as b}from"./chunk-6QJVZ2EJ.js";import"./chunk-SAH4PCID.js";import"./chunk-UM4B3XYO.js";import"./chunk-GMQVBQQH.js";import"./chunk-ROBWBO2Y.js";import"./chunk-BEQZMLMZ.js";export{a as CATEGORY_LIST,b as Projects};
